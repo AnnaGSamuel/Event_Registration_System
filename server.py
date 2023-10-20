bbc1e69a793a18de89148a1e0406b241e74986ab
ATJTECH <angeltjohntech@gmail.com>
2023-10-20 11:00:30 +0530

    changed

--- server.py
+++ server.py
@@ -35,7 +35,13 @@ def submit():
               values = (name,email,start_date)
               cursor.execute(sql,values)
               db.commit()
-              return render_template("reg_success.html")
+              
+              last_inserted_id = cursor.lastrowid
+              cursor.execute("SELECT * FROM your_table_name WHERE id = %s", (last_inserted_id,))
+              last_inserted_row = cursor.fetchone()
+
+              return render_template("reg_success.html",details=last_inserted_row)
+              
     except Exception as e:
         # Handle database-related errors here
         error_message = "An error occurred: " + str(e)
@@ -44,17 +50,3 @@ def submit():
 if(__name__ == "__main__"):
     app.run(debug=True)
 
-'''try:
-        cursor.execute("SELECT COUNT(*) FROM form WHERE event_date = %s", (start_date,))
-        is_date_booked = cursor.fetchone()[0]
-
-        if is_date_booked:
-            error_message = "Already booked. Please choose another date."
-            return render_template("travel.html", error_msg=error_message)
-        else:
-            sql = "INSERT INTO form (name, email, event_date) VALUES (%s, %s, %s)"
-            values = (name, email, start_date)
-            cursor.execute(sql, values)
-            db.commit()
-            return render_template("reg_success.html")'''
-    
